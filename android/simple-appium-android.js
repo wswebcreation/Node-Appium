70bbcc9830436515b79973cd297efc76aa5d6a96
ndmanvar <ndmanvar@yahoo.com>
2016-09-28 09:14:47 -0700

    using https

--- android/simple-appium-android.js
+++ android/simple-appium-android.js
@@ -8,7 +8,7 @@ var wd = require('wd'),
         name: 'Sample Test',
         app: 'http://appium.s3.amazonaws.com/ContactManager.apk'
     },
-    driver = wd.remote("ondemand.saucelabs.com", 80, "YOUR_SAUCE_USERNAME", "YOUR_SAUCE_ACCESS_KEY");
+    driver = wd.remote("https://" + "YOUR_SAUCE_USERNAME" + ":" + "YOUR_SAUCE_ACCESS_KEY" + "@" + "ondemand.saucelabs.com:443/wd/hub");
 
 driver.init(desiredCaps, function(error) {
     if (error) {
